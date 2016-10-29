e2a0bcc9b30affeed2a4b8036716c2e80110a33f
ndsh <hej@julian-h.de>
2016-10-29 02:03:53 +0200

    dc tester update

--- arduino/dc_tester/dc_tester.ino
+++ arduino/dc_tester/dc_tester.ino
@@ -1,9 +1,17 @@
+#include <Servo.h>
+
 #define fadePin1 2
 #define fadePin2 13
 
+Servo myservo0;
+
+
+
 void setup(){
   pinMode(fadePin1, OUTPUT);  
-  pinMode(fadePin2, OUTPUT);  
+  pinMode(fadePin2, OUTPUT); 
+  myservo0.attach(7); 
+  myservo0.write(170);
 }
 
 void loop(){
@@ -15,7 +23,7 @@ void loop(){
     //calculate sin of angle as number between 0 and 255
     int sinOut = constrain((sin(rad) * 128) + 128, 0, 255); 
 
-    analogWrite(fadePin1, sinOut);
+    analogWrite(fadePin1, 255);
     analogWrite(fadePin2, sinOut);
 
     delay(15);
